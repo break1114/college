6c8ed1e8d86a3e29782e6f20c2438cd0bb85ece6
Jay <jopaning@student.42.fr>
2019-09-08 15:27:57 +0200

    activity

---
+++ Activity.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int	main(void)
+{
+	string name, course, year, block;
+
+	cout << "Name:";
+	cin >> name;
+	cout << "Course:";
+	cin >> course;
+	;cout << "Year:";
+	cin >> year;
+	cout << "Block:";
+	cin >> block;
+	cout << "Name :" << name << endl;
+	cout <<  "Course : " << course << endl;
+	cout << "Year : " << year << endl;
+	cout << "Block : " << block << endl;
+
+	return (0);
+}
